24c175e5dbbb007ca3e7222d6e5fc34f1c8256c0
2jeong2 <jeongyi0131@naver.com>
2025-09-16 15:35:52 +0900

    re push

--- src/main/java/edu/ce/fisa/controller/Controller.java
+++ src/main/java/edu/ce/fisa/controller/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("app")
+@RequestMapping("app4")
 public class Controller {
 	//http://localhost:8080/app/get
 	@GetMapping("/get")
